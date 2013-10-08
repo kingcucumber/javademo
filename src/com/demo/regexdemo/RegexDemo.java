e2188965e50751f17b210d78163ee541e35731d0
kingcucumber <jinhua.wang18@gmail.com>
2013-10-08 10:30:08 +0800

    add regex pattern demo.

--- src/com/demo/regexdemo/RegexDemo.java
+++ src/com/demo/regexdemo/RegexDemo.java
@@ -1,5 +1,9 @@
 package com.demo.regexdemo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class RegexDemo {
 
 	/**
@@ -7,15 +11,37 @@ public class RegexDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*String qq = "1234567890";
+		//test1();
+		test2();
+//		test3();
+	}
+
+	private static void test3() {
+		// TODO Auto-generated method stub
+		Boolean res1 = Pattern.matches("\\d{6}","a124214");
+		Boolean res2 = Pattern.matches("\\d{6}","124214");
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+
+	private static void test2() {
+		// TODO Auto-generated method stub
+		Pattern pattern = Pattern.compile("\\d{4}-\\d{1}-\\d{2}");
+		String string = "2013-9-16 2013-9-17";
+		Matcher match = pattern.matcher(string);
+		
+		while(match.find()){
+			System.out.println(match.group(0));
+		}
+	}
+
+	public static void test1() {
+		String qq = "1234567890";
 		
 		String regex = "[1-9][0-9]{4,14}";
 		boolean b = qq.matches(regex);
 		
-		System.out.println(qq + ":" + b);*/
-		
-		
-		
+		System.out.println(qq + ":" + b);
 	}
 
 }
--- src/com/demo/regexdemo/Test.java
+++ src/com/demo/regexdemo/Test.java
@@ -1,5 +1,8 @@
 package com.demo.regexdemo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
 
 	/**
@@ -7,24 +10,75 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test1();
+		//test1();
 		//test2();
 		//test3();
+		test4();
+	}
+
+	private static void test4() {
+		// TODO Auto-generated method stub
+		//切分
+		/*String regex = "\\s+";
+		for(String s : "one\ttwo\n three".split(regex)){
+			System.out.print(s + " ");
+		}*/
+		Pattern pattern = Pattern.compile("\\s+");
+		for(String s : pattern.split("one\ttwo\n three"))
+		System.out.print(s + " ");
 	}
 
 	private static void test3() {
 		// TODO Auto-generated method stub
+		//替换
+		/*String regex = "(\\d{4})-(\\d{2})-(\\d{2})";
+		String repacement = "$2/$3/$1";
+		String str = "2013-09-17 2013-09-18";
+		System.out.println(str.replaceAll(regex, repacement));*/
 		
+		Pattern pattern = Pattern.compile("\\d+\\.\\d{0,2}");
+		Matcher matcher = pattern.matcher("price is 12.99");
+		System.out.println(matcher.replaceAll("\\$$0"));
 	}
 
 	private static void test2() {
 		// TODO Auto-generated method stub
+		//提取
+	/*	Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
+		Matcher matcher = pattern.matcher("2013-09-17 2013-09-18");
 		
+		while(matcher.find()){
+			System.out.println(matcher.group());
+		}*/
+		
+		Pattern pattern = Pattern.compile("(\\d{4})-(\\d{2})-(\\d{2})");
+		Matcher matcher = pattern.matcher("2013-09-17 2013-09-18");
+		/*while(matcher.find()){
+			System.out.println("date:" + matcher.group(0));
+			System.out.println("year:" + matcher.group(1));
+			System.out.println("month:" + matcher.group(2));
+			System.out.println("day:" + matcher.group(3));
+		}*/
+		while (matcher.find()) {
+			for (int i = 0; i < matcher.groupCount(); i++) {
+				System.out.println("date:" + matcher.group(i));
+			}
+		}
 	}
 
 	private static void test1() {
 		// TODO Auto-generated method stub
+		//验证
+		/*boolean flag1 = "2013-09-17".matches("\\d{4}-\\d{2}-\\d{2}");
+		boolean flag2 = Pattern.matches("\\d{4}-\\d{2}-\\d{2}", "2013-09-17");
+		System.out.println(flag1);
+		System.out.println(flag2);*/
 		
+		Pattern pattern = Pattern.compile("\\A\\d{4}-\\d{2}-\\d{2}\\z");
+		String s = "2013-09-17";
+		Matcher matcher = pattern.matcher(s);
+		boolean flag = matcher.find();
+		System.out.println(flag);
 	}
 
 }
